3705b00534aedf6dc7d24e646ed9dda72ba075a8
mkr2177 <manishkumaryadav2177@gmail.com>
2025-05-20 11:29:45 +0530

    Add files via upload

---
+++ preprocessor.py
@@ -0,0 +1,56 @@
+import re
+import pandas as pd
+
+def preprocess(data):
+    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+
+    messages = re.split(pattern, data)[1:]
+    dates = re.findall(pattern, data)
+
+    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+
+    # Try parsing dates with both 2-digit and 4-digit years
+    try:
+        df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %H:%M - ')
+    except ValueError:
+        df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
+
+    df.rename(columns={'message_date': 'date'}, inplace=True)
+
+    users = []
+    messages = []
+    for message in df['user_message']:
+        entry = re.split(r'([\w\W]+?):\s', message)
+        if entry[1:]:  # user name present
+            users.append(entry[1])
+            messages.append(" ".join(entry[2:]))
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+
+    df['user'] = users
+    df['message'] = messages
+    df.drop(columns=['user_message'], inplace=True)
+
+    # Extract additional time-based features
+    df['only_date'] = df['date'].dt.date
+    df['year'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['day_name'] = df['date'].dt.day_name()
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+
+    # Create hourly period labels
+    period = []
+    for hour in df['hour']:
+        if hour == 23:
+            period.append("23-00")
+        elif hour == 0:
+            period.append("00-1")
+        else:
+            period.append(f"{hour}-{hour+1}")
+    df['period'] = period
+
+    return df
